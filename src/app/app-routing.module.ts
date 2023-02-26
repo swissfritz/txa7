4135a7bd7f525ec54d3ed61df0fca3291d3d6a83
Fritz Frei <fritzfrei@Fritzs-MacBook-Pro-2.local>
2023-02-26 09:29:01 +0700

    tan2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -151,6 +151,10 @@ const routes: Routes = [
     path: 'feedback',
     loadChildren: () => import('./feedback/feedback.module').then( m => m.FeedbackPageModule)
   },
+  {
+    path: 'tankentest',
+    loadChildren: () => import('./tankentest/tankentest.module').then( m => m.TankentestPageModule)
+  },
 ];
 
 @NgModule({
